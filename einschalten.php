cf433ba63c361d9f9e4c0bf3906dd735d19beb65
Dominic <Domifry007@aol.com>
2019-06-02 17:46:35 +0200

    Update einschalten.php

--- einschalten.php
+++ einschalten.php
@@ -44,11 +44,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   
     <link rel="stylesheet" href="/main.css">
-    <title>Penthouse Alarmanlage</title>
+    <title>Alarmanlage</title>
   </head>
     <body>
      <nav class=" navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <a class="navbar-brand" href="#">Penthouse Alarmanlage</a>
+      <a class="navbar-brand" href="#">Alarmanlage</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
